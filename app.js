3c4d2f264d93f0f63cc7b39bfbc2b3ab9f98f602
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2018-11-28 14:22:53 -0500

    connection to db in app.js

--- app.js
+++ app.js
@@ -65,7 +65,6 @@ mongoose
 mongoose.Promise = global.Promise;
 //Get the default connection
 const db = mongoose.connection;
-ß;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
